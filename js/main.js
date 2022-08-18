7c398bb2bf81111662d92e600a9f243c720c2ed3
aggusperez0 <agustinperez29@outlook.com>
2022-08-18 19:57:24 -0300

    Agregando JavaScript

--- js/main.js
+++ js/main.js
@@ -6,3 +6,18 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   ScrollReveal().reveal('.news-cards', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
   ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+
+  function ConcatenarMensajes(){
+    var mensajeFinal = "";
+    var SePresionoEscape = true;
+    var ultimaPalabra = "";
+    alert("Finalizara la funcion al presion la tecla 'Esc' o presionando el boton 'Cancelar'");
+    while(ultimaPalabra != null){
+         mensajeFinal += ultimaPalabra;
+         if(mensajeFinal != ""){
+           alert("Mensaje completo: " + mensajeFinal);
+         }
+         ultimaPalabra = prompt("Ingrese una palabra: ");
+      }
+    alert("Mensaje Final: " + mensajeFinal);
+  }
